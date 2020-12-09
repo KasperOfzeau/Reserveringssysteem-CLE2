33a73ba4372b04f4e37fdc19652f605f759aa699
Kasper Beljaars <kasperbeljaars@hotmail.com>
2020-12-09 12:08:37 +0100

    Added login form + checks and database check with redirect

--- adminPanel.php
+++ adminPanel.php
@@ -5,3 +5,11 @@ if($_SESSION["loggedIn"] != true){
     header("Location: admin-login.php");
     exit;
 }
+
+// Check login tiem
+if (time() - $_SESSION['loginTime'] > 300) {
+    header("Location: admin-login.php");
+    exit;
+} else {
+    $_SESSION['loginTime'] = time();
+}
